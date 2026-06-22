233dabb6b17647c8aeee450c7374efd98ddef565
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-22 15:35:24 +0545

    feat:add student model file

---
+++ app/Models/Student.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Course;
+class Student extends Model
+{
+    //
+    protected $fillable=['name'];
+    public function courses(){
+        return $this->belongsToMany(Course::class,'course_student','student_id','course_id');
+    }
+}
