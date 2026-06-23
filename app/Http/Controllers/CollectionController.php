a1f2b73dd21305cf34fd6db66464133a778e6a8d
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-23 13:09:26 +0545

    feat:Implement Laravel collection pluck and filter methods

---
+++ app/Http/Controllers/CollectionController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CollectionController extends Controller
+{
+    //
+    public function index(){
+        $users=collect([
+            ['name'=>'Ram'],
+            ['name'=>'Sita']
+        ]);
+        return view('collection.users',compact('users'));
+    }
+    public function filter(){
+$users = collect([
+    ['name' => 'Ram', 'active' => true],
+    ['name' => 'Sita', 'active' => false],
+    ['name' => 'Hari', 'active' => true],
+    ]);
+    $users=$users->filter(function ($item){
+        return $item['active']==true;
+
+    });
+return view('collection.users',compact('users'));
+    }
+}
