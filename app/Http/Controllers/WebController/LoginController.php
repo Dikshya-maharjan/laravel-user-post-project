f37301ad46cd3d46213edfbf8966d1d41b2d6248
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-07-06 16:40:26 +0545

    feat(auth): implement web login and logout with session authentication

---
+++ app/Http/Controllers/WebController/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\WebController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class LoginController extends Controller
+{
+    //
+    public function index(){
+        return view('/login');
+    }
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+
+        ]);
+
+            if (Auth::attempt([//checks the password and creates the session
+            'email' => $request->email,
+            'password' => $request->password
+        ])) {
+
+            $request->session()->regenerate();
+
+            return redirect('/dashboard');
+        }
+    }
+    
+     public function logout(Request $request)
+    {
+        Auth::logout();//removeauthentication
+        $request->session()->invalidate();//destroy session
+
+        $request->session()->regenerateToken();//create new csrf
+
+        return redirect('/login');
+    }
+
+}
