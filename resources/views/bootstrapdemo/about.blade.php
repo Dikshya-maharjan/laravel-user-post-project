158e362dd1d56acf5eb1a1e60ead467fe0234cd1
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-07-07 21:38:46 +0545

    feat:add about.blade.php file

---
+++ resources/views/bootstrapdemo/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'About Us Page')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-danger">About Our Project</h1>
+            <p class="lead">This page automatically inherits Bootstrap because it connects to the master layout.</p>
+            <button class="btn btn-outline-primary">Bootstrap Button</button>
+        </div>
+    </div>
+</div>
+@endsection
